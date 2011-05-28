3b99e8a2004e84a996a5104e9c10b7f9f42b10ff
Liron Shapira <liron@quixey.com>
2011-05-28 15:00:20 -0700

    log_event call

--- app/guesschat/view.py
+++ app/guesschat/view.py
@@ -1,6 +1,9 @@
 from tipfy.app import Response
 from tipfy.handler import RequestHandler
 from tipfyext.jinja2 import Jinja2Mixin
+from google.appengine.ext import db
+
+from guesschat.writing import eventlogging
 
 class ViewHandler(RequestHandler, Jinja2Mixin):
     def tmpl(self, path, **context):
@@ -13,8 +16,18 @@ class ViewHandler(RequestHandler, Jinja2Mixin):
 
         return self.render_response(path, **params)
 
+    def log_event(self, name, **properties):
+        properties['ip_address'] = self.request.remote_addr
+        properties['path'] = self.request.path
+
+        eventlogging.log_event(name, **properties)
+
 class Home(ViewHandler):
     def get(self):
+        self.log_event(
+            'view home page'
+        )
+
         return self.tmpl(
             'base.tmpl',
             contents='yo:%s' % self.app.debug
