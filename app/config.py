5a135d60e45da1f0bc42d4b81d0393e1ae0ce22d
Liron Shapira <liron@quixey.com>
2011-05-28 13:51:50 -0700

    More setup

--- app/config.py
+++ app/config.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
-"""App configuration."""
 config = {}
+
--- app/guesschat/view.py
+++ app/guesschat/view.py
@@ -4,16 +4,25 @@ from tipfyext.jinja2 import Jinja2Mixin
 
 class ViewHandler(RequestHandler, Jinja2Mixin):
     def tmpl(self, path, **context):
-        return self.render_response(path, **context)
+        params = dict(context)
+
+        if context.get('title'):
+            params['title'] = context['title'] + ' | GuessChat'
+        else:
+            params['title'] = 'GuessChat'
+
+        return self.render_response(path, **params)
 
 class Home(ViewHandler):
     def get(self):
         return self.tmpl(
-            'base.tmpl'
+            'base.tmpl',
+            contents='yo:%s' % self.app.debug
         )
 
 class About(ViewHandler):
     def get(self):
         return self.tmpl(
-            'about.tmpl'
+            'about.tmpl',
+            title='About'
         )
